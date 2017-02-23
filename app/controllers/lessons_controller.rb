8f4351c7fdcc507903abfb3a67c686c83c24a18e
Marshika Szabo <marshika.szabo@gmail.com>
2017-02-23 19:06:08 +0000

    Adjusted enrollment issues

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
+
   def show
   end
 
@@ -7,5 +10,16 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  helper_method :current_course
+  def current_course
+    current_lesson.section.course
+  end
+
+  def require_authorized_for_current_lesson
+    if !current_lesson.section.course.user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'You must be enrolled to view this lesson.'
+    end
   end
 end
